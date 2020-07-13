c3cb75f5459c717d369de249a8c7d743b56330ff
vraghuvaran <vraghuvaran123@gmail.com>
2020-07-13 12:32:02 +0530

    Done with Create Match

--- BoardServer/src/main/java/com/scoreboard/BoardAPI/Controller/MatchController.java
+++ BoardServer/src/main/java/com/scoreboard/BoardAPI/Controller/MatchController.java
@@ -3,6 +3,7 @@ package com.scoreboard.BoardAPI.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.scoreboard.BoardAPI.dto.MatchesToTeams;
 import com.scoreboard.BoardAPI.entity.matchs;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class MatchController {
 		
 	@Autowired
